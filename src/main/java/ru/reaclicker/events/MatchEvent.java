0e1293bc18df0462f4906f219f966295c9129f5d
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-03-28 09:30:12 +0200

    Match click

--- src/main/java/ru/reaclicker/events/MatchEvent.java
+++ src/main/java/ru/reaclicker/events/MatchEvent.java
@@ -1,16 +1,17 @@
 package ru.reaclicker.events;
 
+import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import lombok.extern.slf4j.Slf4j;
 import org.hashids.Hashids;
 import ru.reaclicker.domain.User;
 import ru.reaclicker.events.core.Event;
 import ru.reaclicker.events.core.SessionHolder;
 
-import java.util.HashMap;
-
 /**
  * Created by Artur Belogur on 24.03.17.
  */
+@Slf4j
 public class MatchEvent extends SessionHolder implements Event {
 
     private Hashids hashids;
@@ -27,22 +28,37 @@ public class MatchEvent extends SessionHolder implements Event {
 
             if (searchUsers.size() > 1) {
                 Long enemyId = getEnemyId(user.getId());
+                SocketIOClient enemyClient = server.getClient(searchUsers.get(enemyId));
 
                 client.sendEvent("start-match", userDao.get(enemyId));
-                server.getClient(searchUsers.get(enemyId)).sendEvent("start-match", user);
+                enemyClient.sendEvent("start-match", user);
 
                 searchUsers.remove(user.getId());
                 searchUsers.remove(enemyId);
 
-                client.joinRoom(hashids.encode(user.getId(), enemyId));
+                String room = hashids.encode(user.getId(), enemyId);
+                client.joinRoom(room);
+                enemyClient.joinRoom(room);
             }
         });
 
-        server.addEventListener("match-click", Double.class, (client, score, ackSender) -> {
-            User user = loginUsers.get(client.getSessionId());
-            searchUsers.put(user.getId(), client.getSessionId());
+        server.addEventListener("match-click", Integer.class, (client, score, ackSender) -> {
+            String roomName = client.getAllRooms()
+                    .stream()
+                    .filter(room -> !room.equals(""))
+                    .findFirst()
+                    .orElse(null);
+            if (roomName == null) {
+                return;
+            }
 
-
+            server.getRoomOperations(roomName)
+                    .getClients()
+                    .forEach(user -> {
+                        if (user.getSessionId() != client.getSessionId()) {
+                            user.sendEvent("enemy-click", score);
+                        }
+                    });
         });
     }
 
